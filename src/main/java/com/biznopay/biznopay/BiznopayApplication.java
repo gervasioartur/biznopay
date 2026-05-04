3fad1edc3220dfea14b63a0bcea15721ab748b8a
Gervásio Artur <gervasioartur@outlook.com.br>
2026-05-04 22:22:31 +0200

    chore: adding project structure

---
+++ src/main/java/com/biznopay/biznopay/BiznopayApplication.java
@@ -0,0 +1,13 @@
+package com.biznopay.biznopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiznopayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiznopayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/biznopay/biznopay/BiznopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biznopay.biznopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiznopayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
