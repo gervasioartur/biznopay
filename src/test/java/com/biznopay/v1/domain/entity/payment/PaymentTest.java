b984eefff394837892598fed9b4ec39f5908903f
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 09:12:59 +0200

    test: ensure Payment returns MissingRequiredFieldException if amount is invalid on create payment

--- src/test/java/com/biznopay/v1/domain/entity/payment/PaymentTest.java
+++ src/test/java/com/biznopay/v1/domain/entity/payment/PaymentTest.java
@@ -1,11 +1,13 @@
 package com.biznopay.v1.domain.entity.payment;
 
+import com.biznopay.v1.domain.exception.InvalidAmountException;
 import com.biznopay.v1.domain.exception.MissingRequiredFieldException;
 import com.biznopay.v1.mocks.PaymentMocks;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -23,5 +25,12 @@ public class PaymentTest {
     public void ShouldThrowMissingRequiredFieldExceptionWhenAmountInCentsIsNull() {
         Assertions.assertThrows(MissingRequiredFieldException.class, () -> Payment.create(PaymentMocks.paymentMock().getIdempotencyKey(),
                 null, PaymentMocks.paymentMock().getDescription(), PaymentMocks.paymentMock().getPaymentMethodDetails()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1L,0L,99L,7_500_001L} )
+    public void ShouldThrowInvalidAmountExceptionWhenAmountInCentsIsLessThanZeroOrLesThanMinValueOrGreaterThanMaxValue(Long amountInCents) {
+        Assertions.assertThrows(InvalidAmountException.class, () -> Payment.create(PaymentMocks.paymentMock().getIdempotencyKey(),
+                amountInCents, PaymentMocks.paymentMock().getDescription(), PaymentMocks.paymentMock().getPaymentMethodDetails()));
     }
 }
