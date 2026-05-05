1e9bd8898d5aabbd8fe94f66042b8301ada107e2
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 14:18:27 +0200

    test: ensure Payment throws InvalidFieldException if id is null on with

--- src/test/java/com/biznopay/v1/mocks/Mocks.java
+++ src/test/java/com/biznopay/v1/mocks/Mocks.java
@@ -3,6 +3,7 @@ package com.biznopay.v1.mocks;
 import com.biznopay.v1.domain.entity.payment.Payment;
 import com.biznopay.v1.domain.entity.paymentMethodDetails.MpesaPaymentDetails;
 import com.biznopay.v1.domain.entity.paymentMethodDetails.PaymentMethodDetails;
+import com.biznopay.v1.domain.enums.PaymentMethodType;
 import com.biznopay.v1.usecase.payment.create.CreatePaymentInput;
 
 public class Mocks {
@@ -35,6 +36,6 @@ public class Mocks {
     }
 
     public static CreatePaymentInput createPaymentInputMock() {
-        return new CreatePaymentInput("any_idempotency_key", 100L, "any_description", "847272727");
+        return new CreatePaymentInput("any_idempotency_key", 100L, "any_description", "847272727", PaymentMethodType.MPESA);
     }
 }
