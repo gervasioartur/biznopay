3aa151b01781d00ff07a2e39cf0aca1f60a5aa02
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 08:54:08 +0200

    test: ensure Payment returns MissingRequiredFieldException if amount is null on create payment

--- src/test/java/com/biznopay/v1/domain/entity/payment/PaymentTest.java
+++ src/test/java/com/biznopay/v1/domain/entity/payment/PaymentTest.java
@@ -3,6 +3,7 @@ package com.biznopay.v1.domain.entity.payment;
 import com.biznopay.v1.domain.exception.MissingRequiredFieldException;
 import com.biznopay.v1.mocks.PaymentMocks;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +14,14 @@ public class PaymentTest {
     @ParameterizedTest
     @NullAndEmptySource
     public void ShouldThrowMissingRequiredFieldExceptionWhenIdempotencyKeyIsBlankOrNull(String idempotencyKey) {
-        Assertions.assertThrows(MissingRequiredFieldException.class, () -> Payment.create(idempotencyKey, PaymentMocks.paymentMock().getAmountInCents(),
-                PaymentMocks.paymentMock().getDescription(), PaymentMocks.paymentMock().getPaymentMethodDetails()));
+        Assertions.assertThrows(MissingRequiredFieldException.class, () -> Payment.create(idempotencyKey,
+                PaymentMocks.paymentMock().getAmountInCents(), PaymentMocks.paymentMock().getDescription(),
+                PaymentMocks.paymentMock().getPaymentMethodDetails()));
+    }
+
+    @Test
+    public void ShouldThrowMissingRequiredFieldExceptionWhenAmountInCentsIsNull() {
+        Assertions.assertThrows(MissingRequiredFieldException.class, () -> Payment.create(PaymentMocks.paymentMock().getIdempotencyKey(),
+                null, PaymentMocks.paymentMock().getDescription(), PaymentMocks.paymentMock().getPaymentMethodDetails()));
     }
 }
