e571a06192684efce31dbf1e56e7c3954b08c36b
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 09:13:27 +0200

    chore: adding CODE to the exceptions

--- src/main/java/com/biznopay/v1/domain/exception/InvalidFieldException.java
+++ src/main/java/com/biznopay/v1/domain/exception/InvalidFieldException.java
@@ -1,6 +1,7 @@
 package com.biznopay.v1.domain.exception;
 
 public class InvalidFieldException extends RuntimeException {
+    private final String CODE = "INVALID_FIELD";
 
     public InvalidFieldException(String field, String entity) {
         super("The field " + field + " is invalid!");
--- src/main/java/com/biznopay/v1/domain/exception/MissingRequiredFieldException.java
+++ src/main/java/com/biznopay/v1/domain/exception/MissingRequiredFieldException.java
@@ -1,6 +1,7 @@
 package com.biznopay.v1.domain.exception;
 
 public class MissingRequiredFieldException extends RuntimeException {
+    private final String CODE = "MISSING_REQUIRED_FIELD";
 
     public MissingRequiredFieldException(String field, String entity) {
         super("The field " + field + " is required for " + entity + "!");
