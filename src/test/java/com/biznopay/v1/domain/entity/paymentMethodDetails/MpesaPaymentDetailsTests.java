69d1a44d28ad32e27d5bcf7a47d9a6bb7e06cc3e
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 10:17:17 +0200

    test: ensure MpesaPaymentDetails throws InvalidFieldException if M-Pesa phone number is invalid

--- src/test/java/com/biznopay/v1/domain/entity/paymentMethodDetails/MpesaPaymentDetailsTests.java
+++ src/test/java/com/biznopay/v1/domain/entity/paymentMethodDetails/MpesaPaymentDetailsTests.java
@@ -1,9 +1,11 @@
 package com.biznopay.v1.domain.entity.paymentMethodDetails;
 
+import com.biznopay.v1.domain.exception.InvalidFieldException;
 import com.biznopay.v1.domain.exception.MissingRequiredFieldException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -13,5 +15,11 @@ public class MpesaPaymentDetailsTests {
     @NullAndEmptySource
     public void ShouldThrownMissingRequiredFieldExceptionWhenPhoneNumberIsBlankOrNull(String phoneNumber) {
         Assertions.assertThrows(MissingRequiredFieldException.class, () -> MpesaPaymentDetails.create(phoneNumber));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"any_phone_number", "876965252"})
+    public void ShouldThrownInvalidFieldExceptionWhenPhoneNumberIsInvalid(String phoneNumber){
+        Assertions.assertThrows(InvalidFieldException.class, () -> MpesaPaymentDetails.create(phoneNumber));
     }
 }
