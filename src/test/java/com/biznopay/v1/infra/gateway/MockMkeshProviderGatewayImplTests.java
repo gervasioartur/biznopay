d014e4e6d5f3cccfd518960db06463cf60597aa4
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 14:56:52 +0200

    test: ensure MockMkeshProviderGatewayImpl throws RuntimeException

---
+++ src/test/java/com/biznopay/v1/infra/gateway/MockMkeshProviderGatewayImplTests.java
@@ -0,0 +1,20 @@
+package com.biznopay.v1.infra.gateway;
+
+import com.biznopay.v1.domain.entity.payment.Payment;
+import com.biznopay.v1.mocks.Mocks;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class MockMkeshProviderGatewayImplTests {
+
+    @Test
+    public void ShouldThrowRuntimeExceptionOnSubmitFailure(){
+        Payment payment = Mocks.processingPaymentMock();
+        MockMkeshProviderGatewayImpl gateway = new MockMkeshProviderGatewayImpl();
+        assertThrows(RuntimeException.class, () -> gateway.submit(payment));
+    }
+}
+
