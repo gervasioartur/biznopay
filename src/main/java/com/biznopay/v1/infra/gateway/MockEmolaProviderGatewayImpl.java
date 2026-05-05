27efd5d13ec20a1b66752128da778225955ea05d
gervasiodombo <gervasiorthur@gmil.com>
2026-05-05 14:52:40 +0200

    feat: ensure MockEmolaProviderGatewayImpl return providerPaymentId on submit success

--- src/main/java/com/biznopay/v1/infra/gateway/MockEmolaProviderGatewayImpl.java
+++ src/main/java/com/biznopay/v1/infra/gateway/MockEmolaProviderGatewayImpl.java
@@ -3,9 +3,11 @@ package com.biznopay.v1.infra.gateway;
 import com.biznopay.v1.domain.entity.payment.Payment;
 import com.biznopay.v1.domain.gateway.PaymentProviderGateway;
 
+import java.util.UUID;
+
 public class MockEmolaProviderGatewayImpl implements PaymentProviderGateway {
     @Override
     public String submit(Payment payment) {
-        return "";
+        return UUID.randomUUID().toString();
     }
 }
