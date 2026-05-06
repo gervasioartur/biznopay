8401c59c651571e3042f1ebb4701e239fe02b76c
gervasiodombo <gervasiorthur@gmil.com>
2026-05-06 09:25:47 +0200

    fix: adding component annotation to make sure the right payment method is chosen

--- src/main/java/com/biznopay/v1/infra/gateway/MockEmolaProviderGatewayImpl.java
+++ src/main/java/com/biznopay/v1/infra/gateway/MockEmolaProviderGatewayImpl.java
@@ -2,9 +2,11 @@ package com.biznopay.v1.infra.gateway;
 
 import com.biznopay.v1.domain.entity.payment.Payment;
 import com.biznopay.v1.domain.gateway.PaymentProviderGateway;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class MockEmolaProviderGatewayImpl implements PaymentProviderGateway {
     @Override
     public String submit(Payment payment) {
--- src/main/java/com/biznopay/v1/infra/gateway/MockMkeshProviderGatewayImpl.java
+++ src/main/java/com/biznopay/v1/infra/gateway/MockMkeshProviderGatewayImpl.java
@@ -2,7 +2,9 @@ package com.biznopay.v1.infra.gateway;
 
 import com.biznopay.v1.domain.entity.payment.Payment;
 import com.biznopay.v1.domain.gateway.PaymentProviderGateway;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MockMkeshProviderGatewayImpl implements PaymentProviderGateway {
     @Override
     public String submit(Payment payment) {
