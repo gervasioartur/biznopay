78efe249202caeb0115bdd9f2dd03c9c08611cb3
gervasiodombo <gervasiorthur@gmil.com>
2026-05-06 12:11:37 +0200

    test: ensure FindPymentById PaymentResponse if payment exists on find payment by id

--- src/test/java/com/biznopay/v1/usecase/payment/find/FindPymentByIdTests.java
+++ src/test/java/com/biznopay/v1/usecase/payment/find/FindPymentByIdTests.java
@@ -1,8 +1,10 @@
 package com.biznopay.v1.usecase.payment.find;
 
+import com.biznopay.v1.domain.entity.payment.Payment;
 import com.biznopay.v1.domain.exception.ResourceNotFoundException;
 import com.biznopay.v1.domain.gateway.PaymentGateway;
 import com.biznopay.v1.infra.persistence.jpa.repository.PaymentJpaRepository;
+import com.biznopay.v1.mocks.Mocks;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,8 +28,19 @@ public class FindPymentByIdTests {
     @Test
     public void ShouldThrowResourceNotFoundExceptionWhenPaymentNotFound() {
         UUID paymentId = UUID.randomUUID();
-//        Mockito.when(paymentGateway.findById(paymentId)).thenReturn(Optional.empty());
+        Mockito.when(paymentGateway.findById(paymentId)).thenReturn(Optional.empty());
         FindPymentById findPymentById = setup();
         Assertions.assertThrows(ResourceNotFoundException.class, () -> findPymentById.execute(paymentId));
+    }
+
+    @Test
+    public void ShouldReturnPaymentIfExists(){
+        Payment payment = Mocks.completedMpesaPaymentMock();
+        UUID paymentId = payment.getId().value();
+        Mockito.when(paymentGateway.findById(paymentId)).thenReturn(Optional.of(payment));
+        FindPymentById findPymentById = setup();
+        PaymentResponse paymentResponse = findPymentById.execute(paymentId);
+        Assertions.assertNotNull(paymentResponse);
+        Assertions.assertEquals(paymentId, paymentResponse.id());
     }
 }
